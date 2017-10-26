0e68dea842d044f3aea9bcaf93f9c6dc3e12652b
Emma Lewis <emmarachellewis@gmail.com>
2017-10-26 16:35:11 +0100

    read me

--- README.md
+++ README.md
@@ -1,17 +1,21 @@
 # Flow maps
 
-Flow maps shows movement between geographical areas over time. The default map is a world map that can show movement between countries, but this map can be replaced by other maps to produce movements on different scales at any location.
+Flow maps shows bilateral movement between geographical areas over time. The default map is a world map that can show movement between countries, but this map can be replaced by other maps to produce movements on different scales at any location.
 
 
 ## Usage
 
 ```html
-<div class="flow-map" data-start-year="1990" data-end-year="2005" data-year-gap="5" data-bg-color="#192A3A" data-sending-color="#7E4C7F" data-receiving-color="#23787A" data-title="Title of the Map" data-description="Description goes here" data-sending-text="Sending" data-receiving-text="Receiving" data-text-color="#EFEFEF" data-show-social="true" data-overlay-text-post=" people"></div>
+<div class="flow-map" data-start-year="1990" data-end-year="2005" data-year-gap="5" data-bg-color="#192A3A" data-sending-color="#7E4C7F" data-receiving-color="#23787A" data-title="Title of the Map" data-description="Description goes here" data-sending-text="Sending" data-receiving-text="Receiving" data-text-color="#EFEFEF" data-map-color="#EFEFEF" data-show-social="true" data-overlay-text-post=" people"></div>
 <script src="https://cdn.jsdelivr.net/gh/CodeForAfrica/FlowMaps@1.0/dist/javascript/flow-map.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/CodeForAfrica/FlowMaps@1.0/dist/css/index.css" />
 ```
 
+![flow-maps-screenshot](https://user-images.githubusercontent.com/1282239/32061950-00a8a00c-ba6b-11e7-8a04-f034aab41007.png)
+
 ## API Reference
+
+By default a world map will be displayed. To change this, link to an external TopoJSON file or overwrite the file at `/data/topojson.json` (see [data-topojson](#topojson) for more information). To add your dataset link to an external csv file or overwrite the file at `/data/data.csv` (see [data-data](#data) for more information). 
 
 Flow maps uses [data attributes](https://developer.mozilla.org/en/docs/Web/Guide/HTML/Using_data_attributes) to personalise the visualisation. 
 
@@ -27,9 +31,116 @@ The first year for which data is present
 
 The last year for which data is present
 
+### data-title [required]
+
+#### Description
+
+Title for the map
+
+### data-description [required]
+
+#### Description
+
+Description for the map
+
 ### data-year-gap [optional]
 
 #### Description
 
 The number of years between each dataset. Default: 1
 
+### data-bg-color [optional]
+
+#### Description
+
+Background colour for the map. Default: '#192A3A'
+
+### data-text-color [optional]
+
+#### Description
+
+Text colour for the title, description, switch and social menu. Default: '#EFEFEF'
+
+### data-map-color [optional]
+
+#### Description
+
+Color for the map. Default: '#EFEFEF'
+
+### data-sending-color [optional]
+
+#### Description
+
+Sending colour for the map. Default: '#7E4C7F'
+
+### data-receiving-color [optional]
+
+#### Description
+
+Receiving colour for the map. Default: '#23787A'
+
+### data-sending-text [optional]
+
+#### Description
+
+Label for "sending" direction switch option. Default: 'Sending'
+
+### data-receiving-text [optional]
+
+#### Description
+
+Label for "receiving" direction switch option. Default: 'Receiving'
+
+### data-show-social [optional]
+
+#### Description
+
+Whether or not to show the social menu. If set to true, the icons will populate the share content based on the og/meta tags in your website's `<head>` tag. Default: false
+
+### data-overlay-text-pre [optional]
+
+#### Description
+
+Text to appear on the overlay before the number, e.g. '£'. Default: ''
+
+### data-overlay-text-post [optional]
+
+#### Description
+
+Text to appear on the overlay after the number, e.g. ' people'. Default: ''
+
+### data-ratio [optional]
+
+#### Description
+
+A number to describe the ratio of map width to map height. For a full view of the world map this should always be the default 2.3415. Default: 2.3415
+
+### data-zoom [optional]
+
+#### Description
+
+Magnification factor to display a zoomed map. Default: 1
+
+### data-zoom-x [optional]
+
+#### Description
+
+X position of the centre of zoom. Default: '50%'
+
+### data-zoom-y [optional]
+
+#### Description
+
+Y position of the centre of zoom. Default: '50%'
+
+### data-topojson<a name="topojson"></a> [optional]
+
+#### Description
+
+Link to an external TopoJSON file defining the map to be displayed. By default the world map will be displayed. You can also change the TopoJSON file by simply replacing the file in `/data/topojson.json`. The TopoJSON file must be valid TopoJSON and have exactly one geometry object named `subunits` and with type `GeometryCollection`.
+
+### data-data<a name="data"></a> [optional]
+
+#### Description
+
+Link to an external csv file containing the data to be visualized. By default the data will be pulled from `/data/data.csv`. You can replace this file with your own dataset. The data headings should be as follows: `year,sending_name,receiving_name,amount`.
--- src/scripts/modules/flow-map.js
+++ src/scripts/modules/flow-map.js
@@ -32,8 +32,8 @@ class FlowMap {
         this.overlayTextPre = this.$map.data('overlayTextPre') || ''
         this.overlayTextPost = this.$map.data('overlayTextPost') || ''
         this.zoom = this.$map.data('zoom') || 1
-        this.zoomX = this.$map.data('zoomX') 
-        this.zoomY = this.$map.data('zoomY')
+        this.zoomX = this.$map.data('zoomX') || '50%'
+        this.zoomY = this.$map.data('zoomY') || '50%'
         this.width = 410 * this.$map.data('ratio') || 960
         this.dataArray = []
         this.radiusScale = d3.scaleLinear()
@@ -46,7 +46,7 @@ class FlowMap {
         this.addMarkup()
         this.setupGradient()
         this.svg.append('rect').attr('width', this.width).attr('height', HEIGHT).attr('fill', this.colorBG)
-        if (this.zoomX !== undefined && this.zoomY !== undefined) {
+        if (this.zoom !== 1) {
             $('.flow-map__zoom-wrapper').css({
                 'transform': `scale(${this.zoom})`,
                 'transform-origin': `${this.zoomX} ${this.zoomY}`
@@ -108,6 +108,8 @@ class FlowMap {
     addMarkup() {
         this.$map.css('background', this.colorBG)
         this.$map.css('color', this.colorText)
+        this.$map.css('fill', this.colorText)
+        this.$map.css('stroke', this.colorText)
         const modeColor = this.mode === 'receiving' ? this.colorReceiving : this.colorSending
         let header = '<div class="flow-map__header">'
         if (this.showSocial) {
@@ -116,15 +118,15 @@ class FlowMap {
                     <span class="social-media__title-text">Share</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="social-media__title-icon" viewBox="0 0 20.82 14.61">
                         <title>Share</title>
-                        <path fill="#fff" d="M20.32,6.08,12.58.17a.8.8,0,0,0-.5-.17.83.83,0,0,0-.83.83V4.16H9.17c-6,0-8.49,3.75-9.16,9.53a.81.81,0,0,0,.83.92.93.93,0,0,0,.71-.37,8.77,8.77,0,0,1,8-4.25h1.71v3.29a.83.83,0,0,0,.83.83.8.8,0,0,0,.5-.17L20.36,8A1.23,1.23,0,0,0,20.32,6.08Z"/>
+                        <path stroke="none" d="M20.32,6.08,12.58.17a.8.8,0,0,0-.5-.17.83.83,0,0,0-.83.83V4.16H9.17c-6,0-8.49,3.75-9.16,9.53a.81.81,0,0,0,.83.92.93.93,0,0,0,.71-.37,8.77,8.77,0,0,1,8-4.25h1.71v3.29a.83.83,0,0,0,.83.83.8.8,0,0,0,.5-.17L20.36,8A1.23,1.23,0,0,0,20.32,6.08Z"/>
                     </svg>
                 </a>
                 <div class="social-media__buttons">
                     <span id="share-close-button" class="social-media__close">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 51.47 51.47">
                             <title>Close</title>
-                            <line fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="3" x1="50.29" y1="1.18" x2="1.18" y2="50.29"/>
-                            <line fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="3" x1="50.29" y1="50.29" x2="1.18" y2="1.18"/>
+                            <line fill="none" stroke-miterlimit="10" stroke-width="3" x1="50.29" y1="1.18" x2="1.18" y2="50.29"/>
+                            <line fill="none" stroke-miterlimit="10" stroke-width="3" x1="50.29" y1="50.29" x2="1.18" y2="1.18"/>
                         </svg>
                     </span>
                     <ul>
@@ -132,7 +134,7 @@ class FlowMap {
                             <a id="twitter-button" class="social-media__link" href="#0">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="social-media__icon" viewBox="0 0 16.7 13.57">
                                     <title>social_tw</title>
-                                    <path fill="#fff" d="M16.7,1.61a6.84,6.84,0,0,1-2,.54A3.44,3.44,0,0,0,16.24.25a6.83,6.83,0,0,1-2.17.83A3.43,3.43,0,0,0,8.23,4.21,9.73,9.73,0,0,1,1.16.63,3.43,3.43,0,0,0,2.22,5.2,3.41,3.41,0,0,1,.67,4.78a3.43,3.43,0,0,0,2.75,3.4,3.48,3.48,0,0,1-1.55.06,3.42,3.42,0,0,0,3.2,2.38A6.91,6.91,0,0,1,0,12a9.7,9.7,0,0,0,5.25,1.54A9.69,9.69,0,0,0,15,3.38,7,7,0,0,0,16.7,1.61Z"/>
+                                    <path stroke="none" d="M16.7,1.61a6.84,6.84,0,0,1-2,.54A3.44,3.44,0,0,0,16.24.25a6.83,6.83,0,0,1-2.17.83A3.43,3.43,0,0,0,8.23,4.21,9.73,9.73,0,0,1,1.16.63,3.43,3.43,0,0,0,2.22,5.2,3.41,3.41,0,0,1,.67,4.78a3.43,3.43,0,0,0,2.75,3.4,3.48,3.48,0,0,1-1.55.06,3.42,3.42,0,0,0,3.2,2.38A6.91,6.91,0,0,1,0,12a9.7,9.7,0,0,0,5.25,1.54A9.69,9.69,0,0,0,15,3.38,7,7,0,0,0,16.7,1.61Z"/>
                                 </svg>
                                 <span class="social-media__text">Twitter</span>
                             </a>
@@ -141,7 +143,7 @@ class FlowMap {
                             <a id="facebook-button" class="social-media__link" href="#0">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="social-media__icon" viewBox="0 0 10.05 21.53">
                                     <title>social_fb</title>
-                                    <path fill="#fff" d="M6.68,7.05V5.2c0-1,.09-1.49,1.48-1.49H10V0h-3C3.48,0,2.23,1.8,2.23,4.83V7.05H0v3.71H2.23V21.53H6.68V10.76h3L10,7.05Z"/>
+                                    <path stroke="none" d="M6.68,7.05V5.2c0-1,.09-1.49,1.48-1.49H10V0h-3C3.48,0,2.23,1.8,2.23,4.83V7.05H0v3.71H2.23V21.53H6.68V10.76h3L10,7.05Z"/>
                                 </svg>
                                 <span class="social-media__text">Facebook</span>
                             </a>
@@ -150,7 +152,7 @@ class FlowMap {
                             <a class="social-media__link" href="mailto:?subject=...">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="social-media__icon" viewBox="0 0 23.51 23.5">
                                     <title>social_mail</title>
-                                    <path fill="#fff" d="M23.34.32,23.27.24,23.18.16A.72.72,0,0,0,22.55,0L.56,10.93l0,0a.71.71,0,0,0-.5.51.73.73,0,0,0,0,.38H0v0a.76.76,0,0,0,.1.21l.08.09.12.11.12.06.1.05,6.67,3.18,3.88,7.37a42.65,42.65,0,0,1,.11.22.77.77,0,0,0,.11.12l.09.08a.77.77,0,0,0,.21.1h0a.73.73,0,0,0,.38,0,.71.71,0,0,0,.5-.5l0,0L23.48,1A.72.72,0,0,0,23.34.32ZM2.43,11.65l16.4-8.14L7.62,14.13Zm9.39,9.5-3.15-6L20.22,4.22Z"/>
+                                    <path stroke="none" d="M23.34.32,23.27.24,23.18.16A.72.72,0,0,0,22.55,0L.56,10.93l0,0a.71.71,0,0,0-.5.51.73.73,0,0,0,0,.38H0v0a.76.76,0,0,0,.1.21l.08.09.12.11.12.06.1.05,6.67,3.18,3.88,7.37a42.65,42.65,0,0,1,.11.22.77.77,0,0,0,.11.12l.09.08a.77.77,0,0,0,.21.1h0a.73.73,0,0,0,.38,0,.71.71,0,0,0,.5-.5l0,0L23.48,1A.72.72,0,0,0,23.34.32ZM2.43,11.65l16.4-8.14L7.62,14.13Zm9.39,9.5-3.15-6L20.22,4.22Z"/>
                                 </svg>
                                 <span class="social-media__text">Email</span>
                             </a>
@@ -160,7 +162,7 @@ class FlowMap {
                               data-action="share/whatsapp/share">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="social-media__icon" viewBox="0 0 41.93 41.93">
                                     <title>WhatsApp</title>
-                                    <path fill="#fff" d="M41.93,20.43A20.62,20.62,0,0,1,11.4,38.31L0,41.93,3.72,31a20.21,20.21,0,0,1-3-10.55,20.59,20.59,0,0,1,41.17,0ZM21.35,3.25A17.26,17.26,0,0,0,4,20.43,17,17,0,0,0,7.34,30.5L5.18,36.87l6.65-2.11A17.32,17.32,0,0,0,38.66,20.43,17.26,17.26,0,0,0,21.35,3.25Zm10.4,21.88c-.13-.21-.46-.33-1-.58s-3-1.46-3.45-1.63-.8-.25-1.14.25-1.3,1.63-1.6,2-.59.38-1.09.13a13.81,13.81,0,0,1-4.06-2.48,15.1,15.1,0,0,1-2.81-3.47c-.29-.5,0-.77.22-1s.51-.58.76-.88a3.36,3.36,0,0,0,.5-.84.91.91,0,0,0,0-.88C18,15.45,16.94,13,16.52,12s-.84-.83-1.13-.83-.63,0-1,0a1.86,1.86,0,0,0-1.35.63,5.6,5.6,0,0,0-1.77,4.18,9.69,9.69,0,0,0,2.06,5.18c.25.33,3.49,5.55,8.62,7.56S27.12,30,28,29.89s3-1.21,3.41-2.38A4.17,4.17,0,0,0,31.74,25.13Z"/>
+                                    <path stroke="none" d="M41.93,20.43A20.62,20.62,0,0,1,11.4,38.31L0,41.93,3.72,31a20.21,20.21,0,0,1-3-10.55,20.59,20.59,0,0,1,41.17,0ZM21.35,3.25A17.26,17.26,0,0,0,4,20.43,17,17,0,0,0,7.34,30.5L5.18,36.87l6.65-2.11A17.32,17.32,0,0,0,38.66,20.43,17.26,17.26,0,0,0,21.35,3.25Zm10.4,21.88c-.13-.21-.46-.33-1-.58s-3-1.46-3.45-1.63-.8-.25-1.14.25-1.3,1.63-1.6,2-.59.38-1.09.13a13.81,13.81,0,0,1-4.06-2.48,15.1,15.1,0,0,1-2.81-3.47c-.29-.5,0-.77.22-1s.51-.58.76-.88a3.36,3.36,0,0,0,.5-.84.91.91,0,0,0,0-.88C18,15.45,16.94,13,16.52,12s-.84-.83-1.13-.83-.63,0-1,0a1.86,1.86,0,0,0-1.35.63,5.6,5.6,0,0,0-1.77,4.18,9.69,9.69,0,0,0,2.06,5.18c.25.33,3.49,5.55,8.62,7.56S27.12,30,28,29.89s3-1.21,3.41-2.38A4.17,4.17,0,0,0,31.74,25.13Z"/>
                                 </svg>
                                 <span class="social-media__text">WhatsApp</span>
                             </a>
@@ -180,26 +182,30 @@ class FlowMap {
         this.$map.append(header)
         this.$map.append($('<div class="flow-map__visualisation"><div class="flow-map__svg-wrapper"><div class="flow-map__zoom-wrapper"></div></div></div>'))
 
-        this.svg = d3.select('.flow-map__zoom-wrapper').append('svg').attr('viewBox', `0 0 ${this.width} ${HEIGHT}`).attr('class', 'flow-map__svg')
-        let timeline = `<div class="timeline" style="background:${this.colorMap};"><span class="timeline__play"></span><div class="timeline__years">`
+        this.svg = d3.select('.flow-map__zoom-wrapper').append('svg').attr('viewBox', `0 0 ${this.width} ${HEIGHT}`).attr('class', 'flow-map__svg').attr('stroke', 'none')
         this.numberOfYears = (this.endYear - this.startYear) / this.yearGap + 1
-        this.yearFraction = 1 / this.numberOfYears
-        if (this.yearFraction * (this.$window.outerWidth() - 55) < 50) {
-            let maxPerRow = Math.floor(this.$window.outerWidth() - 55) / 50
-            let rows = Math.ceil(this.numberOfYears / maxPerRow)
-            this.multipleTimelineLines = true
-            this.yearFraction = 1 / Math.floor(this.numberOfYears / rows)
-        }
-        this.opacities = shuffle(Array.from(new Array((this.endYear - this.startYear) / this.yearGap + 1), (val,index) => (index + 1) * this.yearFraction))
-        const hex = hexToRgb(modeColor)
-        for (let i = this.startYear; i < this.endYear + 1; i += this.yearGap) {
-            const className = i === this.startYear ? 'timeline__year timeline__year--active' : 'timeline__year'
-            timeline += `<span class="${className}" data-year="${i}" style="width:${100 * this.yearFraction}%;background:rgba(${hex.r},${hex.g},${hex.b},${this.opacities[(i - this.startYear) / this.yearGap]})">${i}</span>`
-        }
-        timeline += '</div></div>'
         this.$header = $('.flow-map__header')
         this.$visualisation = $('.flow-map__visualisation')
-        this.$visualisation.append($(timeline))
+
+        if (this.numberOfYears > 1) {
+            let timeline = `<div class="timeline" style="background:${this.colorMap};"><span class="timeline__play"></span><div class="timeline__years">`
+            this.yearFraction = 1 / this.numberOfYears
+            if (this.yearFraction * (this.$window.outerWidth() - 55) < 50) {
+                let maxPerRow = Math.floor(this.$window.outerWidth() - 55) / 50
+                let rows = Math.ceil(this.numberOfYears / maxPerRow)
+                this.multipleTimelineLines = true
+                this.yearFraction = 1 / Math.floor(this.numberOfYears / rows)
+            }
+            this.opacities = shuffle(Array.from(new Array((this.endYear - this.startYear) / this.yearGap + 1), (val,index) => (index + 1) * this.yearFraction))
+            const hex = hexToRgb(modeColor)
+            for (let i = this.startYear; i < this.endYear + 1; i += this.yearGap) {
+                const className = i === this.startYear ? 'timeline__year timeline__year--active' : 'timeline__year'
+                timeline += `<span class="${className}" data-year="${i}" style="width:${100 * this.yearFraction}%;background:rgba(${hex.r},${hex.g},${hex.b},${this.opacities[(i - this.startYear) / this.yearGap]})">${i}</span>`
+            }
+            timeline += '</div></div>'    
+            this.$visualisation.append($(timeline))
+        }
+
         this.$svgWrapper = $('.flow-map__svg-wrapper')
         this.$svg = $('.flow-map__svg')
         this.$timeline = $('.timeline')
@@ -284,8 +290,9 @@ class FlowMap {
             .projection(projection)
 
         const BASE_URL = window.location.href.split('/').slice(0, -1).join('/')
-        let url = this.$map.data('topojson') ? this.$map.data('topojson') : BASE_URL + '/data/world-map.json'
-        d3.json(url, (error, map) => {
+        let mapUrl = this.$map.data('topojson') ? this.$map.data('topojson') : BASE_URL + '/data/topojson.json'
+        let dataUrl = this.$map.data('data') ? this.$map.data('data') : BASE_URL + '/data/data.csv'
+        d3.json(mapUrl, (error, map) => {
             const COUNTRIES = topojson.feature(map, map.objects.subunits).features
 
             const COUNTRY_PATHS = this.svg.selectAll('.flow-map__country')
@@ -300,7 +307,7 @@ class FlowMap {
                     .on('mouseover', this.activateCountry.bind(this))
                     .on('mouseout', this.deactivateCountry.bind(this))
 
-            d3.csv(BASE_URL + '/data/immigration-fixed.csv', (error, data) => {
+            d3.csv(dataUrl, (error, data) => {
                 let maxAmount = 0
                 let minAmount = 0
                 let sendingCountryEntry
